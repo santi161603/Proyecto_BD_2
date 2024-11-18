55ca30d575a1061b869e515956f2693930c1f26f
santi161603 <116686563+santi161603@users.noreply.github.com>
2024-11-18 00:15:31 -0500

    Proyect-Create

---
+++ app/src/main/java/com/uni/proyecto/proyecto_bd_2/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.uni.proyecto.proyecto_bd_2.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
